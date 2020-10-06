300aa2d47f5ca5a3d576efaf5887569a2413cd0e
Alejandro Cantarero <cantarer@gmail.com>
2020-10-06 13:34:15 -0700

    fix code formatting

--- tipjar/NamiAppDelegate.swift
+++ tipjar/NamiAppDelegate.swift
@@ -2,7 +2,7 @@ import Nami
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func namiSetup() {
-         let namiConfig = NamiConfiguration(appPlatformID: "YOUR_NAMI_APP_ID_HERE")        
+        let namiConfig = NamiConfiguration(appPlatformID: "YOUR_NAMI_APP_PLATFORM_ID_HERE")        
         Nami.configure(namiConfig: namiConfig )
     }
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
